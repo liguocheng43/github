e2e1d0ecd3474dad7f6132636ee0dd92ae8d7daa
liguocheng <liguocheng43@qq.com>
2018-08-27 19:36:31 +0800

    第一次

---
+++ lucene/src/main/java/com/itheima/Book.java
@@ -0,0 +1,49 @@
+package com.itheima;
+
+public class Book {
+    private Integer id;
+    private String bookName;
+    private Float price;
+    private String pic;
+    private String bookDesc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getBookDesc() {
+        return bookDesc;
+    }
+
+    public void setBookDesc(String bookDesc) {
+        this.bookDesc = bookDesc;
+    }
+}
